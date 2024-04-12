584e585153a3223624c0cd4bc37f8c9ec3fe9d3c
PraxisMapper <114430030+PraxisMapper@users.noreply.github.com>
2024-04-12 19:32:06 -0400

    Remove test foreground service code

--- plugin/src/main/java/org/praxismapper/gps/GodotAndroidPlugin.kt
+++ plugin/src/main/java/org/praxismapper/gps/GodotAndroidPlugin.kt
@@ -2,59 +2,19 @@ package org.praxismapper.gps
 
 import android.annotation.TargetApi
 import android.app.Activity
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.Service
 import android.content.Context
-import android.content.Intent
-import android.content.pm.ServiceInfo
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
-import android.hardware.SensorManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Build
-import android.os.IBinder
 import android.util.Log
 import android.view.Display
 import android.view.View
-import androidx.core.app.NotificationCompat
 import org.godotengine.godot.Dictionary
 import org.godotengine.godot.Godot
-import org.godotengine.godot.GodotLib
 import org.godotengine.godot.plugin.GodotPlugin
 import org.godotengine.godot.plugin.SignalInfo
 import org.godotengine.godot.plugin.UsedByGodot
-
-class TestForegroundService: Service() {
-
-    @TargetApi(Build.VERSION_CODES.Q)
-    override fun onBind(p0: Intent?): IBinder? {
-        TODO("Not yet implemented")
-    }
-
-    @TargetApi(Build.VERSION_CODES.Q)
-    override fun onCreate(){
-        Log.d("PraxisForeground", "Service called, activating foreground")
-        val channel = NotificationChannel(
-            "864",
-            "PraxisForegroundChannel",
-            NotificationManager.IMPORTANCE_HIGH
-        )
-        channel.description = "PraxisMapper channel for foreground service notification"
-
-        var notificationManager = getSystemService<NotificationManager>(NotificationManager::class.java)
-        notificationManager.createNotificationChannel(channel)
-
-        var note = NotificationCompat.Builder(this, "864").setSmallIcon(R.drawable.ic_notify_icon).build()
-        note.category = Notification.CATEGORY_NAVIGATION
-        startForeground(864, note, ServiceInfo.FOREGROUND_SERVICE_TYPE_LOCATION)
-        Log.d("PraxisForeground", "Call completed")
-    }
-}
 
 class GodotAndroidPlugin(godot: Godot): GodotPlugin(godot), LocationListener {
 
@@ -87,8 +47,6 @@ class GodotAndroidPlugin(godot: Godot): GodotPlugin(godot), LocationListener {
 
     @TargetApi(Build.VERSION_CODES.S)
     override fun onMainCreate(activity: Activity?): View? {
-        Log.d("PraxisGPS", "Entered OnMainCreate")
-        //NOTE: these might already exist on the godot object passed in to the constructor.
         locman = activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
         return super.onMainCreate(activity)
     }
@@ -109,13 +67,6 @@ class GodotAndroidPlugin(godot: Godot): GodotPlugin(godot), LocationListener {
         this.runOnUiThread() {
             locman.requestLocationUpdates("gps", 500, 0.5f, this);
         }
-
-        //Moved this here so it'll only get called once there's STUFF that exists.
-        Log.d("PraxisGPS", "create foreground service")
-        var intent = Intent(activity, TestForegroundService::class.java)
-        intent.putExtra("name", "Praxis Foreground Test")
-        activity?.startForegroundService(intent)
-        Log.d("PraxisGPS", "foreground service call started")
     }
 
     override fun getPluginSignals(): Set<SignalInfo> {
@@ -133,7 +84,6 @@ class GodotAndroidPlugin(godot: Godot): GodotPlugin(godot), LocationListener {
     }
 
     override fun onLocationChanged(location: Location) {
-            Log.d("PraxisGPS", "Location Changed")
             val longitude = location.longitude.toFloat()
             val latitude = location.latitude.toFloat()
             val accuracy = location.accuracy
